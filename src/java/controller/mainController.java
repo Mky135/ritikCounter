a74ca6788e3f7340e6fbb37e6ab30e84db61c9df
Miguel Flores <mky.flores@hotmail.com>
2018-05-24 11:48:35 -0500

    added classes needed

---
+++ src/java/controller/mainController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainController implements Initializable
+{
+    public AnchorPane pane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
+}
