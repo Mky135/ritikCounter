5e8423f35881dbff2b8d3440ce88cb2c8db8d9d8
MDW01 <michaeldwagner@me.com>
2018-05-31 23:19:09 -0500

    Optimized Database

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -2,47 +2,47 @@ import javax.swing.*;
 import java.io.File;
 import java.sql.*;
 
-public class Database {
+class Database {
+    private static String dbConn = "jdbc:sqlite:";
+    private static String dbPath = "src/main/resources/";
     private static String dbName = "Ritik.sqlite";
-    private static String dbPath = "jdbc:sqlite:src/main/resources/";
-    private static String Database = dbPath + dbName;
-    private static File dbFile = new File(Database);
+    private static String Database = dbConn + dbPath + dbName;
+    private static File dbFile = new File(dbPath + dbName);
+    private int ritik;
 
     int getRitik() {
         return ritik;
     }
 
-    private int ritik;
-
     private static void createDB() {
 
         String url = Database;
 
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
+        try (Connection connection = DriverManager.getConnection(url)) {
+            if (connection != null) {
+                DatabaseMetaData meta = connection.getMetaData();
+                System.out.println("SQL Driver " + meta.getDriverName());
                 System.out.println("A new database has been created.");
+                Statement statement = connection.createStatement();
+                statement.setQueryTimeout(30);
+                statement.executeUpdate("create table if not exists Counter(pk INTEGER PRIMARY KEY, Ritik INTEGER not null)");
+                statement.executeUpdate("INSERT INTO Counter(Ritik) VALUES(0)");
             }
-
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Deprecated
     void connectDB() {
 
-        if (!dbFile.isFile()){
-            createDB();
-        }
+        if (!dbFile.isFile()) { createDB(); }
 
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(Database);
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            statement.executeUpdate("create table if not exists Counter(pk INTEGER PRIMARY KEY, Ritik INTEGER not null)");
         }
         catch(SQLException e) {
             JOptionPane.showMessageDialog(null, "Error: Could Not Connect to Database.\n" + "Please Check Your Internet Connection and Try Again.");
@@ -52,19 +52,14 @@ public class Database {
         finally {
             closeConnection(connection);
         }
-
-        refreshDB(false);
     }
 
-    @Deprecated
-    public void updateDB() {
+    void updateDB() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(Database);
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-
-            statement.executeUpdate("create table if not exists Counter(pk INTEGER PRIMARY KEY, Ritik INTEGER not null)");
             String sql = "UPDATE Counter SET Ritik=Ritik+1 WHERE pk = 1";
             PreparedStatement pstmt = connection.prepareStatement(sql);
             pstmt.executeUpdate();
@@ -76,12 +71,9 @@ public class Database {
         finally {
             closeConnection(connection);
         }
-
-        refreshDB(true);
     }
 
-    @Deprecated
-    private void refreshDB(boolean refresh) {
+    void refreshDB() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(Database);
